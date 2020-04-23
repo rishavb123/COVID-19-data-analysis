0230d3ff4e9a55369c62a0390a57272866555c2b
Rishav Bhagat <rishavb123@gmail.com>
2020-04-23 04:10:54 -0400

    created transforms from y-space to t(y)-space

---
+++ transforms.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+def integral(ys):
+    return [sum(ys[0:i]) for i in range(1, len(ys))]
+
+def derivative(ys):
+    dy = []
+    for i in range(1, len(ys) - 1):
+        dy.append((ys[i + 1] - ys[i - 1]) / 2)
+    return dy
+
+def second_derivative(ys):
+    dy = []
+    for i in range(1, len(ys) - 1):
+        dy.append(ys[i + 1] + ys[i - 1] - 2 * ys[i])
+    return dy
+
+def smooth(ys, starting_point_ratio=0.3):
+    c_points = np.fft.rfft(ys)
+    c_points[int(starting_point_ratio * len(c_points)):] = 0
+    return np.fft.irfft(c_points)
+
+def composite(transforms):
+    def transform(ys):
+        for t in transforms:
+            ys = t(ys)
+        return ys
+    return transform
+
+def filter_y(f):
+    return lambda ys: [y for y in ys if f(y)]
+
+def remove_outliers(ys):
+    mean = np.mean(ys)
+    std = np.std(ys)
+    return [y for y in ys if (mean - 2 * std < y < mean + 2 * std)]
+
+def polyfit(best_fit_degree):
+    def transform(ys):
+        p = np.polyfit(list(range(len(ys))), ys, best_fit_degree)
+        return [sum([(i ** j) * p[best_fit_degree - j] for j in range(best_fit_degree + 1)]) for i in range(len(ys))]
+    return transform
