8ec7d1fbbbf54230f50c7df78b57025cc241655d
Rishav Bhagat <rishavb123@gmail.com>
2020-04-22 22:30:00 -0400

    created update data script

--- update_data.bat
+++ update_data.bat
@@ -1,9 +1,14 @@
-@echo off
-deldir data
+REM @echo off
+rmdir data /s /q
 git clone https://github.com/CSSEGISandData/COVID-19.git
 rename COVID-19 data
 cd data
-deldir .git
+rmdir .git /s /q
+rmdir archived_data /s /q
+rmdir who_covid_19_situation_reports /s /q
+del README.md
+mv csse_covid_19_data/* .
+rmdir csse_covid_19_data /s /q
 cd ..
 git add data
 git commit -m "updated data from John Hopkins github repo"
