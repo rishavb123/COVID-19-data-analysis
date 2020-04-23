e73331689861d8a0d164d4eb718ea91fe5909dee
Rishav Bhagat <rishavb123@gmail.com>
2020-04-23 04:22:51 -0400

    added transform functionality

--- find_fit.py
+++ find_fit.py
@@ -13,12 +13,13 @@ country = "United Kingdom"
 max_best_fit_degree = 15
 threshold = 1e-2
 show = False
+transform = None
 
 last_mse = 0
 orig_mse = 0
 
 for best_fit_degree in range(1, max_best_fit_degree):
-    p, mse = plot_time_series_data(files, names, country=country, best_fit_degree=best_fit_degree)
+    p, mse = plot_time_series_data(files, names, country=country, transform=transform best_fit_degree=best_fit_degree)
     print("Best Fit Degree:", best_fit_degree)
     print("Weights:", p)
     print("Mean Squared Error:", mse)
