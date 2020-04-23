b459ebafb2ff45cdf277493ac9207eb71055e15a
Rishav Bhagat <rishavb123@gmail.com>
2020-04-23 05:03:31 -0400

    added regresssion to logorithmic data

--- plot_data.py
+++ plot_data.py
@@ -20,8 +20,8 @@ names = [
 country = None
 
 plot_time_series_data(files, names, country=country)
-plot_time_series_data(files, names, np.log, "Log(Cases)", country=country)
-plot_time_series_data(files, names, composite([integral]), "\int(Cases)*d(Days)", country=country, best_fit_degree=2)
+plot_time_series_data(files, names, np.log, "Log(Cases)", country=country, best_fit_degree=1)
+plot_time_series_data(files, names, composite([integral]), "\int(Cases)*d(Days)", country=country)
 plot_time_series_data(files, names, composite([derivative, smooth]), "d(Cases)/d(Days)", country=country)
 # plot_time_series_data(files, names, composite([derivative, smooth, derivative, remove_outliers]), "d^2(Cases)/d(Days)^2", country=country)
 
