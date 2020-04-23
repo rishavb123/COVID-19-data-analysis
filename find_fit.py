f67fb841e56633f00cfcf104984c5f87957a812f
Rishav Bhagat <rishavb123@gmail.com>
2020-04-23 04:17:36 -0400

    created script to find best poly fit degree

---
+++ find_fit.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+
+from plots import plot_time_series_data
+
+files = [
+    "./data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+]
+names = [
+    "Global Confirmed Cases"
+]
+
+country = "United Kingdom"
+max_best_fit_degree = 15
+threshold = 1e-2
+show = False
+
+last_mse = 0
+orig_mse = 0
+
+for best_fit_degree in range(1, max_best_fit_degree):
+    p, mse = plot_time_series_data(files, names, country=country, best_fit_degree=best_fit_degree)
+    print("Best Fit Degree:", best_fit_degree)
+    print("Weights:", p)
+    print("Mean Squared Error:", mse)
+    if best_fit_degree != 1:
+        r = (last_mse - mse) / orig_mse
+        print("Improvement:", r)
+        if r < threshold: break
+    else:
+        orig_mse = mse
+    last_mse = mse
+    print()
+
+if show:
+    plt.show()
