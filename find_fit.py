32ff96d7ee910fee1d49033a2a2cb0acc040d01c
Rishav Bhagat <rishavb123@gmail.com>
2020-04-23 04:23:11 -0400

    minor bug fix

--- find_fit.py
+++ find_fit.py
@@ -19,7 +19,7 @@ last_mse = 0
 orig_mse = 0
 
 for best_fit_degree in range(1, max_best_fit_degree):
-    p, mse = plot_time_series_data(files, names, country=country, transform=transform best_fit_degree=best_fit_degree)
+    p, mse = plot_time_series_data(files, names, country=country, transform=transform, best_fit_degree=best_fit_degree)
     print("Best Fit Degree:", best_fit_degree)
     print("Weights:", p)
     print("Mean Squared Error:", mse)
