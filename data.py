38a7593cdac51fd504ef22b02fe83e7fc86d18ab
Rishav Bhagat <rishavb123@gmail.com>
2020-04-24 19:20:29 -0400

    back to global

--- data.py
+++ data.py
@@ -3,13 +3,13 @@ def read(filename, usa=False, country=None):
     lines = f.readlines()
     ind = 13 if usa else 3 
     ys = [0 for _ in lines[0].split(',')[ind:]]
-    if usa: ys += [0]
+    if usa: 
+        ys += [0]
     for line in lines[1:]:
         l = line.split(',')
         if country == None or l[1].lower() == country.lower():
             l = l[ind:]
             for i in range(len(ys)):
-                if not l[i].isdigit(): l[i] = '1'
                 ys[i] += float(l[i])
     f.close()
     return ys
--- plot_data.py
+++ plot_data.py
@@ -4,36 +4,36 @@ import matplotlib.pyplot as plt
 from plots import plot_time_series_data
 from transforms import *
 
-# files = [
-#     "./data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv",
-#     "./data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv",
-#     "./data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"
-# ]
-# names = [
-#     "Global Confirmed Cases",
-#     "Global Death Cases",
-#     "Global Recovered Case"
-# ]
-
 files = [
-    "./data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv",
-    # "./data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv"
+    "./data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv",
+    "./data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv",
+    "./data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"
 ]
 names = [
-    "United States Confirmed Cases",
-    # "United States Death Cases"
+    "Global Confirmed Cases",
+    "Global Death Cases",
+    "Global Recovered Case"
 ]
+
+# files = [
+#     "./data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv",
+#     # "./data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv"
+# ]
+# names = [
+#     "United States Confirmed Cases",
+#     # "United States Death Cases"
+# ]
 
 # country = "Italy"
 # country = "United Kingdom"
 country = None
-usa = True
+usa = False
 
 plot_time_series_data(files, names, country=country, usa=usa)
 plot_time_series_data(files, names, np.log, "Log(Cases)", country=country, best_fit_degree=1)
 # plot_time_series_data(files, names, composite([integral]), "\int(Cases)*d(Days)", country=country)
 # plot_time_series_data(files, names, composite([derivative, smooth]), "d(Cases)/d(Days)", country=country)
-plot_time_series_data(files[:1], names[:1], composite([growth_rate, remove_outliers]), "Growth Rate of Cases", best_fit_degree=3, country=country, scatter=True)
+plot_time_series_data(files[:1], names[:1], composite([growth_rate, remove_outliers]), "Growth Rate of Cases", best_fit_degree=4, country=country, scatter=True)
 # plot_time_series_data(files, names, composite([derivative, smooth, derivative, remove_outliers]), "d^2(Cases)/d(Days)^2", country=country)
 
 
