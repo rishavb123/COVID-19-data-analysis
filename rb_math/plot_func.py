dbbb779d11454647c4a789b555d1cd8194644efe
Rishav Bhagat <rishavb123@gmail.com>
2020-04-24 05:36:06 -0400

    added label parameter

--- rb_math/plot_func.py
+++ rb_math/plot_func.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot(f, start, end, n=100):
+def plot(f, start, end, n=100, label=None):
     xs = np.arange(start, end, (end - start) / n)
     ys = [f(x) for x in xs]
-    plt.plot(xs, ys)
+    plt.plot(xs, ys, label=labelw)
