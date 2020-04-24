005cdb376bd4ac927c629fa352927bce0992c0c9
Rishav Bhagat <rishavb123@gmail.com>
2020-04-24 05:36:23 -0400

    fixed typo

--- rb_math/plot_func.py
+++ rb_math/plot_func.py
@@ -4,4 +4,4 @@ import matplotlib.pyplot as plt
 def plot(f, start, end, n=100, label=None):
     xs = np.arange(start, end, (end - start) / n)
     ys = [f(x) for x in xs]
-    plt.plot(xs, ys, label=labelw)
+    plt.plot(xs, ys, label=label)
