b935b5869d417927255573d35ee8b3d6db16ea04
Rishav Bhagat <rishavb123@gmail.com>
2020-04-24 18:13:59 -0400

    created support for reading US data

--- data.py
+++ data.py
@@ -1,12 +1,15 @@
-def read(filename, country=None):
+def read(filename, usa=False, country=None):
     f = open(filename)
     lines = f.readlines()
-    ys = [0 for _ in lines[0].split(',')[3:]]
+    ind = 13 if usa else 3 
+    ys = [0 for _ in lines[0].split(',')[ind:]]
+    if usa: ys += [0]
     for line in lines[1:]:
         l = line.split(',')
         if country == None or l[1].lower() == country.lower():
-            l = l[3:]
+            l = l[ind:]
             for i in range(len(ys)):
+                if not l[i].isdigit(): l[i] = '1'
                 ys[i] += float(l[i])
     f.close()
     return ys
