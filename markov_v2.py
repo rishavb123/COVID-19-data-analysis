3a8c7d25a3bd2d41bef7e41498229406e24459dd
Rishav Bhagat <rishavb123@gmail.com>
2020-04-28 05:28:51 -0400

    exponential portion working

--- markov_v2.py
+++ markov_v2.py
@@ -27,7 +27,7 @@ for county in counties:
     rs = []
     r_ds = []
     r_rs = []
-    s_is = []
+    ss = []
     plt.figure()
     for i in range(len(cases[county]) - 1):
         n_d = deaths[county][i + 1]
@@ -40,45 +40,46 @@ for county in counties:
         n = cases[county][i] - d - e
         p = populations[county] - n
 
-        A = np.array([
-            [n, 0, 0, p],
-            [0, n, 0, 0],
-            [0, 0, n, 0],
-            [n, 0, 0, 0]
-        ])
-        b = np.array([n_n, n_d - d, n_e - e, n + p - n_p])
-
         if n != 0:
 
             r_d = (n_d - d) / n
             r_r = (n_e - e) / n
             r = (n + p - n_p) / n
-            s_i = (n_n - n * r) / p
 
             rs.append(r)
             r_ds.append(r_d)
             r_rs.append(r_r)
-            s_is.append(s_i)
 
     r = np.mean(rs)
+
+    for i in range(len(cases[county]) - 1):
+        n_d = deaths[county][i + 1]
+        n_e = 0
+        n_n = cases[county][i + 1] - n_d - n_e
+        n_p = populations[county] - n_n
+
+        d = deaths[county][i]
+        e = 0
+        n = cases[county][i] - d - e
+        p = populations[county] - n
+
+        if n != 0:
+
+            ss.append((n_n - r * n) * populations[county] / n ** 2)
+
     r_d = np.mean(r_ds)
     r_r = np.mean(r_rs)
-    w = np.polyfit(list(range(len(s_is))), s_is, deg = 4)
+    s = np.mean(ss)
 
-    s = lambda i: sum([w[len(w) - 1 - j] * i ** j for j in range(len(w))])
-
-    plt.scatter(list(range(len(s_is))), s_is)
-    plt.plot(list(range(len(s_is))), [s(i) for i in range(len(s_is))])
-    plt.show()
-
-    T = lambda i: np.array([
-        [r, 0, 0, s(i)],
+    T = np.array([
+        [r, 0, 0, s],
         [r_d, 1, 0, 0],
         [r_r, 0, 1, 0],
         [0, 0, 0, 0]
     ])
 
-    print("r:", r, "r_d:", r_d, "r_r:", r_r)
+    print(T)
+    print("r:", r, "r_d:", r_d, "r_r:", r_r, "s: ", s)
     
     plt.plot(cases[county], label="Cases")
     pred = []
@@ -89,10 +90,12 @@ for county in counties:
                 d = deaths[county][i]
                 e = 0
                 n = cases[county][i] - d - e
-                p = populations[county] - n
+                p = n ** 2 / populations[county]
                 pred.append(np.array([n, d, e, p]))
             else:
-                pred.append(np.matmul(T(i), pred[-1]))
+                state_vec = np.matmul(T, pred[-1])
+                state_vec[3] = state_vec[0] ** 2 / populations[county]
+                pred.append(state_vec)
         else: counter += 1
     plt.plot(list(range(counter, len(pred) + counter)), [t[0] for t in pred], label="Markov Chain")
     plt.figure()
